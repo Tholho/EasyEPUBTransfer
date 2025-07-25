1bfe1e3dbc6aa8949d1aa11abe074a02394a83e9
tholho <tholhodev@gmail.com>
2025-07-25 05:41:27 +0200

    Improved readability with larger font size and icons addition

--- app/src/main/java/com/example/easyepubtransfer/ui/states/DefaultView.kt
+++ app/src/main/java/com/example/easyepubtransfer/ui/states/DefaultView.kt
@@ -1,35 +1,71 @@
 package com.example.easyepubtransfer.ui.states
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.easyepubtransfer.R
+import com.example.easyepubtransfer.ui.theme.EasyEPUBTransferTheme
 
 @Composable
 fun DefaultView(message: String = stringResource(R.string.defaultMessage)) {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp),
-        contentAlignment = Alignment.Center
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = message,
-                style = MaterialTheme.typography.titleLarge,
-                textAlign = TextAlign.Center,
-            )
-        }
+        Spacer(modifier = Modifier.weight(0.3f))
+
+        Icon(
+            painter = painterResource(R.drawable.usb_24px),
+            contentDescription = stringResource(id = R.string.usb_icon_content_description),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(96.dp)
+        )
+
+        Spacer(modifier = Modifier.weight(0.2f))
+
+        Text(
+            text = message,
+            style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.weight(0.3f))
+
+        Icon(
+            painter = painterResource(R.drawable.cable_24px),
+            contentDescription = stringResource(id = R.string.cable_icon_content_description),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(64.dp)
+        )
+
+        Spacer(modifier = Modifier.weight(0.05f))
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultViewPreview() {
+    EasyEPUBTransferTheme {
+        DefaultView()
     }
 }
